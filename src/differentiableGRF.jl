92adc99c59dbed3ded46b709b9fc5ce0a0f9abe6
Felix Benning <felix.benning@gmail.com>
2022-10-06 15:44:15 +0200

    rewrite TaylorCovariance

--- src/differentiableGRF.jl
+++ src/differentiableGRF.jl
@@ -1,26 +1,30 @@
 
-struct DifferentiableGRF{T<:Number, N}
-	grf::GaussianRandomField{T, N}
+struct DifferentiableGRF{Order,T<:Number,N}
+    grf::GaussianRandomField{T,N}
 end
 
-function DifferentiableGRF(cov::CovarianceKernel{T,N}; jitter = 10*eps(T)) where {T<: Number, N}
-	return DifferentiableGRF{T,N}(
-		GaussianRandomField(
-			Kernels.TaylorCovariance{1, T, N}(cov),
-			jitter=jitter
-		)
-	)
+function DifferentiableGRF{Order}(cov::CovarianceKernel{T,N}; jitter=10 * eps(T)) where {Order, T<:Number,N}
+    return DifferentiableGRF{Order,T,N}(
+        GaussianRandomField(
+            Kernels.TaylorCovariance{Order}(cov),
+            jitter=jitter
+        )
+    )
 end
 
-function (dgrf::DifferentiableGRF{T, N})(x::AbstractVector{T}) where {T,N}
-	res = dgrf.grf(x)
-	return (val=res[1], gradient=res[2:end])
+function DifferentiableGRF(cov::CovarianceKernel{T,N}; jitter=10 * eps(T)) where {T<:Number,N}
+    return DifferentiableGRF{1}(cov, jitter)
 end
 
-function conditionalExpectation(dgrf::DifferentiableGRF{T,N}) where {T,N}
-	cond = conditionalExpectation(dgrf.grf)
-	return x::AbstractVector{T} -> begin
-		res = cond(x)
-		return (val=res[1], gradient=res[2:end])
-	end
+function (dgrf::DifferentiableGRF{1,T,N})(x::AbstractVector{T}) where {T,N}
+    res = dgrf.grf(x)
+    return (val=res[1], gradient=res[2:end])
+end
+
+function conditionalExpectation(dgrf::DifferentiableGRF{1,T,N}) where {T,N}
+    cond = conditionalExpectation(dgrf.grf)
+    return x::AbstractVector{T} -> begin
+        res = cond(x)
+        return (val=res[1], gradient=res[2:end])
+    end
 end
--- src/kernels.jl
+++ src/kernels.jl
@@ -5,111 +5,101 @@ using Zygote: Zygote
 import ..StationaryKernel, ..IsotropicKernel, ..CovarianceKernel
 
 @inline function (k::StationaryKernel{T,N})(
-	x::Union{AbstractVector{T}, Vector{Zygote.ForwardDiff.Dual{Nothing, T, N}}},
-	y::Union{AbstractVector{T}, Vector{Zygote.ForwardDiff.Dual{Nothing, T, N}}}
+    x::Union{AbstractVector{T},Vector{Zygote.ForwardDiff.Dual{Nothing,T,N}}},
+    y::Union{AbstractVector{T},Vector{Zygote.ForwardDiff.Dual{Nothing,T,N}}}
 ) where {T,N}
-	@boundscheck length(x) == N || throw(ArgumentError(
-		"The first input is of size $(length(x)), but should be of size $N"))
-	@boundscheck length(y) == N || throw(ArgumentError(
-		"The second input is of size $(length(y)), but should be of size $N"))
-	return @inbounds k(x-y) 
+    @boundscheck length(x) == N || throw(ArgumentError(
+        "The first input is of size $(length(x)), but should be of size $N"))
+    @boundscheck length(y) == N || throw(ArgumentError(
+        "The second input is of size $(length(y)), but should be of size $N"))
+    return @inbounds k(x - y)
 end
 
 @inline function (k::IsotropicKernel{T,N})(
-	d::Union{AbstractVector{T},Vector{Zygote.ForwardDiff.Dual{Nothing, T, N}}}
+    d::Union{AbstractVector{T},Vector{Zygote.ForwardDiff.Dual{Nothing,T,N}}}
 ) where {T,N}
-	@boundscheck length(d) == N || throw(ArgumentError(
-		"The input is of size $(length(d)), but should be of size $N"))
-	return sqNormEval(k, LinearAlgebra.dot(d,d))
+    @boundscheck length(d) == N || throw(ArgumentError(
+        "The input is of size $(length(d)), but should be of size $N"))
+    return sqNormEval(k, LinearAlgebra.dot(d, d))
 end
 
-struct TaylorCovariance{Order, T, N} <: CovarianceKernel{T,N}
-	k::CovarianceKernel{T,N}
+struct TaylorCovariance{Order,T,N,K<:CovarianceKernel{T,N}} <: CovarianceKernel{T,N}
+    k::K
 end
 
-function (cov::TaylorCovariance{1,T,N})(x::AbstractVector{T}, y::AbstractVector{T}) where {T,N}
-	return taylor1Covariance(cov.k, x, y)
+function TaylorCovariance{Order}(k::K) where {Order,T,N,K<:CovarianceKernel{T,N}}
+    return TaylorCovariance{Order,T,N,K}(k)
 end
 
-@inline function taylor1Covariance(k::StationaryKernel{T,N}, x::AbstractVector{T}, y::AbstractVector{T}) where {T,N}
-	@boundscheck length(x) == N || throw(ArgumentError(
-		"The first input is of size $(length(x)), but should be of size $N"))
-	@boundscheck length(y) == N || throw(ArgumentError(
-		"The second input is of size $(length(y)), but should be of size $N"))
-	return @inbounds taylor1Covariance(k, x-y)
+@inline function (cov::TaylorCovariance{1,T,N,K})(x::AbstractVector{T}, y::AbstractVector{T}) where {T,N,K<:StationaryKernel{T,N}}
+    return cov(x - y)
 end
 
 """
-	Auto-diff Fallback of the taylor1Covariance for StationaryKernel
+	Auto-diff Fallback for the TaylorCovariance{1,T,N,StationaryKernel{T,N}}
 """
-@inline function taylor1Covariance(k::StationaryKernel{T,N}, d::AbstractVector{T}) where {T,N}
-	@boundscheck length(d) == N || throw(ArgumentError(
-		"The input is of size $(length(d)), but should be of size $N"))
+@inline function (cov::TaylorCovariance{1,T,N,K})(d::AbstractVector{T}) where {T,N,K<:StationaryKernel{T,N}}
+    @boundscheck length(d) == N || throw(ArgumentError(
+        "The input is of size $(length(d)), but should be of size $N"))
 
-	result = Matrix{T}(undef, N+1, N+1)
-	val, grad = @inbounds Zygote.withgradient(k, d)
-	grad = first(grad) # only one input
-	result[1,1] = val 
-	result[2:end, 1] = grad
-	result[1, 2:end] = - grad
-	result[2:end, 2:end] = @inbounds -Zygote.hessian(k, d)
-	return result
+    result = Matrix{T}(undef, N + 1, N + 1)
+    val, grad = @inbounds Zygote.withgradient(cov.k, d)
+    grad = first(grad) # only one input
+    result[1, 1] = val
+    result[2:end, 1] = grad
+    result[1, 2:end] = -grad
+    result[2:end, 2:end] = @inbounds -Zygote.hessian(cov.k, d)
+    return result
 end
 
 
 """
-	Auto-diff Fallback of the taylor1Covariance for IsotropicKernel
+	Auto-diff Fallback for TaylorCovariance{1,T,N,IsotropicKernel{T,N}}
 """
-@inline function taylor1Covariance(k::IsotropicKernel{T,N}, d::AbstractVector{T}) where {T,N}
-	@boundscheck length(d) == N || throw(ArgumentError(
-		"The input is of size $(length(d)), but should be of size $N"))
+@inline function (cov::TaylorCovariance{1,T,N,K})(d::AbstractVector{T}) where {T,N,K<:IsotropicKernel{T,N}}
+    @boundscheck length(d) == N || throw(ArgumentError(
+        "The input is of size $(length(d)), but should be of size $N"))
 
-	result = Matrix{T}(undef, N+1, N+1)
-	val, grad_1dim = Zygote.withgradient(x->sqNormEval(k, x), LinearAlgebra.dot(d,d))
+    result = Matrix{T}(undef, N + 1, N + 1)
+    val, grad_1dim = Zygote.withgradient(x -> sqNormEval(cov.k, x), LinearAlgebra.dot(d, d))
 
-	result[1,1] = val 
+    result[1, 1] = val
 
-	grad_1dim = first(grad_1dim) # only one input
-	grad = 2*grad_1dim*d
-	result[2:end, 1] = grad
-	result[1, 2:end] = - grad
+    grad_1dim = first(grad_1dim) # only one input
+    grad = 2 * grad_1dim * d
+    result[2:end, 1] = grad
+    result[1, 2:end] = -grad
 
-	hess_1dim = Zygote.hessian(x->sqNormEval(k,x), LinearAlgebra.dot(d,d))
-	hess = 4*hess_1dim * d*d' + 2*grad_1dim* LinearAlgebra.I
-	result[2:end, 2:end] = - hess 
-	return result
+    hess_1dim = Zygote.hessian(x -> sqNormEval(cov.k, x), LinearAlgebra.dot(d, d))
+    hess = 4 * hess_1dim * d * d' + 2 * grad_1dim * LinearAlgebra.I
+    result[2:end, 2:end] = -hess
+    return result
 end
 
-struct SquaredExponential{T<:Number, N} <: IsotropicKernel{T,N}
-	lengthScale::T
+struct SquaredExponential{T<:Number,N} <: IsotropicKernel{T,N}
+    lengthScale::T
 end
 
 @inline function sqNormEval(
-	k::SquaredExponential{T,N},
-	d::Union{T, Zygote.ForwardDiff.Dual}
+    k::SquaredExponential{T,N},
+    d::Union{T,Zygote.ForwardDiff.Dual}
 ) where {T,N}
-	return exp(-d/(2*k.lengthScale^2))
-end
-
-
-function squaredExponential(x,y)
-	return exp(-LinearAlgebra.norm(x-y)^2/2)
+    return exp(-d / (2 * k.lengthScale^2))
 end
 
 
 function gaussKernel(x)
-	exp(-x^2/2)
+    exp(-x^2 / 2)
 end
 
-function sqExpKernelWithGrad(x::AbstractVector{T},y::AbstractVector{T}) where T<: Number
-	d = x-y
-	dim = length(d)
-	result = Matrix{T}(undef, dim+1, dim+1)
-	result[1,1] = 1
-	result[2:end, 1] = - d
-	result[1, 2:end] = d
-	result[2:end, 2:end] = (LinearAlgebra.I - d * transpose(d))
-	return result * gaussKernel(LinearAlgebra.norm(d))
+@inline function (cov::TaylorCovariance{1,T,N,K})(d::AbstractVector{T}) where {T,N,K<:SquaredExponential{T,N}}
+    dim = length(d)
+    result = Matrix{T}(undef, dim + 1, dim + 1)
+    result[1, 1] = 1
+    result[2:end, 1] = -d
+    result[1, 2:end] = d
+    result[2:end, 2:end] = (LinearAlgebra.I - d * transpose(d))
+    return result * gaussKernel(LinearAlgebra.norm(d))
 end
 
 end # module
