3b20d79f67592d407f5cf77d780dd7bbc6783fb3
Felix Benning <felix.benning@gmail.com>
2022-11-04 14:21:04 +0100

    implement algorithm 2 (untested)

--- src/blockPackedLowerTriangular.jl
+++ src/blockPackedLowerTriangular.jl
@@ -5,7 +5,7 @@ using LinearAlgebra: LinearAlgebra
 
 	Blocksize kxk
 """
-struct BlockPackedLowerTri{T, k} <: AbstractArray{T,2}
+struct BlockPackedLowerTri{T, k} <: AbstractMatrix{T}
 	data::Vector{T}
 	used_rows::Int
 end
@@ -47,14 +47,43 @@ end
 end
 		
 """
-	\\(L,v)
+	\\(L,B)
 
 finds and returns x such that L * x = v
 """
-@inline function \(A::BlockPackedLowerTri{T,k}, v::AbstractVector{T}) where {T,k}
+@inline function \(L::BlockPackedLowerTri{T,k}, B::AbstractMatrix{T}) where {T,k}
 	v_len = length(v)
 	@boundscheck v_len == L.used_rows || throw("L is of size $(L.used_rows) while v is of size $(v_len)")
 	result = Vector{T}(undef, v_len)
 	p = 0
 	# TODO
+end
+
+"""
+	Solve X * L^T = B in-place
+
+	L is (kn x kn) BlockPackedLowerTri{T,k} for some n
+	B is kx(kn) matrix appended to L.data, changed in place into X.
+
+	Finally L.used_rows is incremented by k.
+"""
+@inline function _extending_aligned_solve!(L::BlockPackedLowerTri{T,k}) where {T,k}
+	b_size = k*k
+	n = L.used_rows / k
+	for row in 0:(n-1)
+		t = (g(n) + row) * b_size
+		C = reshape(L.data[t+1:t+b_size], k, k) # +1 because 1-based indexing
+		for idx in 0:(row-1)
+			loc = (g(row) + idx) * b_size
+			L = reshape(L.data[loc+1:loc+b_size], k, k) # +1 because 1-based indexing
+			loc = (g(n) + idx) * b_size
+			Γ = reshape(L.data[loc+1:loc+b_size], k, k) # +1 because 1-based indexing
+			C -= Γ * transpose(L)
+		end
+		loc = (g(row) + row) * b_size
+		L = reshape(L.data[loc+1: loc + b_size], k, k) # +1 because still f**ing 1-based indexing
+		C /= transpose(L)
+		# L.data[t+1:t+b_size] = reshape(C, :) # needed? should be inplace - fix above!
+	end
+	L.used_rows += k
 end
