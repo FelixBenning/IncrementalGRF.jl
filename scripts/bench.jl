52cbaac90b49b45ea56987ef66913dfade57edbf
Alex <alex@MacBookAir>
2022-11-07 17:41:43 +0100

    Fix benchmark execution on new configurations

--- scripts/bench.jl
+++ scripts/bench.jl
@@ -6,9 +6,12 @@ include("../test/benchmark_suite.jl")
 
 if isfile("test/manual_local_benchmark.json")
     old_results = B.load("test/manual_local_benchmark.json")[1]
+else 
+    old_results = NaN
 end
-
 results = runTunedSuite("test/params.json")
 
+
 B.judge(B.minimum(results), B.minimum(old_results))
+
 
