3ce138dbf01bee82ba2a9f3c83cae4b8fb31c5c2
Alex <alex@MacBookAir>
2022-11-07 17:15:42 +0100

    Update bench.jl to allow execution on new configurations

--- scripts/bench.jl
+++ scripts/bench.jl
@@ -1,9 +1,13 @@
+Pkg.activate(".")
 using IncrementalGRF, Pkg
 Pkg.activate("test")
 
 include("../test/benchmark_suite.jl")
 
-old_results = B.load("test/manual_local_benchmark.json")[1]
+if isfile("test/manual_local_benchmark.json")
+    old_results = B.load("test/manual_local_benchmark.json")[1]
+end
+
 results = runTunedSuite("test/params.json")
 
 B.judge(B.minimum(results), B.minimum(old_results))
