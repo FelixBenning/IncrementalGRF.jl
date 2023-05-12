355efb67e0cd97c299b5b4ade83610648c49dd53
Felix Benning <felix.benning@gmail.com>
2023-05-12 19:14:51 +0200

    implementation which seems to be working

--- scripts/experiments.jl
+++ scripts/experiments.jl
@@ -1,7 +1,26 @@
 using KernelFunctions: Kernel, KernelFunctions as KF
 using OneHotArrays: OneHotVector
 import ForwardDiff as FD
+import LinearAlgebra as LA
 
+function partial(fun, dim, partials=())
+	if !isnothing(local next = iterate(partials))
+		ii, state = next
+		return partial(
+			x -> FD.derivative(0) do dx
+				fun(x .+ dx * OneHotVector(ii, dim))
+			end,
+			dim,
+			Base.rest(partials, state),
+		)
+	end
+	return fun 
+end
+
+function partial(k, dim; partials_x=(), partials_y=())
+	local f(x,y) = partial(t -> k(t,y), dim, partials_x)(x)
+	return (x,y) -> partial(t -> f(x,t), dim, partials_y)(y)
+end
 struct Pt{Dim}
 	pos::AbstractArray
 	partial
@@ -15,34 +34,11 @@ for T in subtypes(Kernel)
 	(k::T)(x, y::Pt{Dim}) where {Dim} = evaluate_(k, Pt(x), y)
 end
 
-function evaluate_(k::Kernel, x::Pt{Dim}, y::Pt{Dim}) where {Dim}
-	if !isnothing(local next = iterate(x.partial))
-		ii, state = next # take partial derivative in direction ii
-		return FD.derivative(0) do dx
-			evaluate_( # recursion
-				k,  
-				Pt(
-					x.pos .+ dx * OneHotVector(ii, Dim), # directional variation
-					partial=Base.rest(x.partial, state) # remaining partial derivatives
-				),
-				y
-			)
-		end
-	end
-	if !isnothing(local next = iterate(y.partial))
-		jj, state = next # take partial derivative in direction jj
-		return FD.derivative(0) do dy
-			evaluate_( # recursion
-				k,
-				x,
-				Pt(
-					y.pos .+ dy * OneHotVector(jj, Dim), # directional variation
-					partial=Base.rest(y.partial, state) # remaining partial derivatives
-				)
-			)
-		end
-	end
-	k(x.pos, y.pos)
+function evaluate_(k::T, x::Pt{Dim}, y::Pt{Dim}) where {Dim, T<:Kernel}
+	return partial(
+		k, Dim,
+		partials_x=x.partial, partials_y=y.partial
+	)(x.pos, y.pos)
 end
 
 k = KF.MaternKernel()
@@ -50,8 +46,13 @@ k = KF.MaternKernel()
 k([1],[1])
 k([1], Pt([1], partial=(1,1)))
 k(Pt([1], partial=1), [2]) # ∂ₓk(x,y)
-k(Pt([1], partial=(1)), Pt([2], partial=1))# ∂ₓk(x,y)
+k(Pt([1,2], partial=(1)), Pt([1,2], partial=2))# ∂ₓk(x,y)
 
-k(Pt(1, partial=(1,)), 2)
 
 k = KF.SEKernel()
+
+kappa(x) = exp(- LA.dot(x,x)/2)
+
+
+g = partial(k,1, partials_x=2, partials_y=0)
+g(1,1)
